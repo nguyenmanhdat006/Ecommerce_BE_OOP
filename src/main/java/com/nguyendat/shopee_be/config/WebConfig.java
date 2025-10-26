daa75c235f0837b764f2523979cfdae18a57674b
thang2k6adu <thang2k6adu@gmail.com>
2025-10-26 09:59:56 +0700

    [CONFIG] setup CORS for call API

--- src/main/java/com/nguyendat/shopee_be/config/WebConfig.java
+++ src/main/java/com/nguyendat/shopee_be/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.nguyendat.shopee_be.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.nio.file.Path;
@@ -17,5 +18,15 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath + "/");
+    }
+
+    // Enable CORS globally
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // apply to all endpoints
+                .allowedOriginPatterns("*") // allow multi origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
