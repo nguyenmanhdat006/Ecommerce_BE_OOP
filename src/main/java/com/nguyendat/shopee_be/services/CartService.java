84a6538fb14d5a6907316fc69ad7581603ff82cf
nguyendat <manhdatvbhp@gmail.com>
2025-11-01 14:35:24 +0700

    [ADD] user profile

--- src/main/java/com/nguyendat/shopee_be/services/CartService.java
+++ src/main/java/com/nguyendat/shopee_be/services/CartService.java
@@ -1,13 +1,13 @@
 package com.nguyendat.shopee_be.services;
 
-import com.nguyendat.shopee_be.entities.Cart;
+import com.nguyendat.shopee_be.dto.CartDto;
 import java.util.List;
 import java.util.UUID;
 
 public interface CartService {
-    List<Cart> findAll();
-    Cart findById(UUID id);
-    Cart create(Cart cart);
-    Cart update(UUID id, Cart cart);
+    List<CartDto> findAll();
+    CartDto findById(UUID id);
+    CartDto create(CartDto cartDto);
+    CartDto update(UUID id, CartDto cartDto);
     void deleteById(UUID id);
 }
