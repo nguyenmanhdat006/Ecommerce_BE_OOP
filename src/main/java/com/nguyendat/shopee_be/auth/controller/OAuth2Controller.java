189ede9bddbf24bfed27e5b24056f07918998d5b
thang2k6adu <thang2k6adu@gmail.com>
2025-11-13 23:17:45 +0700

    fix: env

--- src/main/java/com/nguyendat/shopee_be/auth/controller/OAuth2Controller.java
+++ src/main/java/com/nguyendat/shopee_be/auth/controller/OAuth2Controller.java
@@ -5,6 +5,7 @@ import com.nguyendat.shopee_be.auth.entities.User;
 import com.nguyendat.shopee_be.auth.services.OAuth2Service;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
