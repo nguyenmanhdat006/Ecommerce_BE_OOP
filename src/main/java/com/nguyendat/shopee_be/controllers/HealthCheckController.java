7a81a3c0e1e891ce1886a495df6c1c02ee3eb442
thang2k6adu <thang2k6adu@gmail.com>
2025-12-04 16:40:36 +0700

    feature: health check api

---
+++ src/main/java/com/nguyendat/shopee_be/controllers/HealthCheckController.java
@@ -0,0 +1,29 @@
+package com.nguyendat.shopee_be.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/health")
+@CrossOrigin
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> healthCheck() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("timestamp", LocalDateTime.now());
+        response.put("message", "Service is running");
+        
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
+
